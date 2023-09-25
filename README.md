dbbdf4b7e5d5623b1f8b4bd7cdb749faa90ba3f2
Tor Flatebo <tor@flatebo.org>
2023-09-24 22:17:08 -0500

    First cut at trying to handle the weird error handling from the underlying API library

--- README.md
+++ README.md
@@ -3,11 +3,18 @@ https://www.koyeb.com/tutorials/dockerize-and-deploy-a-laravel-application-to-pr
 Docker stuff
 ```
 docker build -t tflatebo/unifi-parental-control:api -f Dockerfile .
-docker run --rm -t -p 8051:80 tflatebo/unifi-parental-control:api
+docker run --rm -t --env-file /tmp/api-variables.env -p 8051:80 tflatebo/unifi-parental-control:api
 docker push tflatebo/unifi-parental-control:api
 ```
 
 Use the API on the command line
 ```
+Get a list of all rules
+curl -s -X GET -H "Content-Type: application/json" http://0.0.0.0:8051/api/firewallrules/
+
+Get info about one rule
+
+
+Turn a rule on
 curl -s -X PUT -H "Content-Type: application/json" -d '{"enabled": true}' http://192.168.1.10:8051/api/firewallrules/<rule id> | jq .
 ```
--- app/Http/Controllers/API/FirewallRulesController.php
+++ app/Http/Controllers/API/FirewallRulesController.php
@@ -16,51 +16,21 @@ class FirewallRulesController extends Controller
      */
     public function index()
     {
-        //
-        return response()->json($this -> listrules());
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        // not used
-    }
+        // login, if this returns false, we 
+        // hit the rate limit and should return an error
+        $connection = $this->login();
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        // not used
-    }
+        if ($connection) {
+            $result = $connection->list_firewallrules();
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        return response()->json($this -> getrule($id));
-    }
+            if ($result) { // only return response if not empty, second API call could have had an error
+                return response()->json($result);
+            }
+        }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        // not used
+        // if we got here, that means we didn't return a useful result, so send an error
+        return response()->json(['error' => 500, 'message' => 'Unifi API client returned an error (see https://github.com/Art-of-WiFi/UniFi-API-client/blob/master/src/Client.php#L205'], 500);
     }
 
     /**
@@ -70,57 +40,44 @@ class FirewallRulesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, $rule_id)
     {
         $payload = array('enabled' => ($request->json()->get('enabled')));
-        return response()->json($this -> updaterule($id, $payload));
+
+        // login, if this returns false, we 
+        // hit the rate limit and should return an error
+        $connection = $this->login();
+
+        if ($connection) { // if the login was successful, proceed
+            $method = 'PUT';
+
+            $result = $connection->custom_api_request('/api/s/' . env("UNIFI_SITE") . '/rest/firewallrule/' . $rule_id, $method, $payload);
+
+            if ($result) { // only return response if not empty, second API call could have had an error
+                return response()->json($result);
+            }
+        }
+
+        // if we got here, that means we didn't return a useful result, so send an error
+        return response()->json(['error' => 500, 'message' => 'Unifi API client returned an error (see https://github.com/Art-of-WiFi/UniFi-API-client/blob/master/src/Client.php#L205'], 500);
     }
 
     /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * initialize the Unifi API connection class, log in to the controller and request the alarms collection
+     * (this example assumes you have already assigned the correct values to the variables used)
+     * 
+     * Return: unifi_client if the loging worked, null if there was an error
+     * 
      */
-    public function destroy($id)
+    private function login()
     {
-        // not used
-    }
-
-    private function listrules()
-    {
-        /**
-         * initialize the Unifi API connection class, log in to the controller and request the alarms collection
-         * (this example assumes you have already assigned the correct values to the variables used)
-         */
         $unifi_connection = new Unifi_Client(env("UNIFI_USER"), env("UNIFI_PASS"), env("UNIFI_URI"), env("UNIFI_SITE"), env("UNIFI_VERSION"), false);
         $login            = $unifi_connection->login();
-        $results          = $unifi_connection->list_firewallrules(); // returns a PHP array containing alarm objects
 
-        return $results;
+        if ($login) { // login was successful, return the connection to be used for the next request
+            return $unifi_connection;
+        } else { // login was unsuccessful, return the problem
+            return $login;
+        }
     }
-
-    private function getrule($id)
-    {
-        $method = "GET";
-
-        $unifi_connection = new Unifi_Client(env("UNIFI_USER"), env("UNIFI_PASS"), env("UNIFI_URI"), env("UNIFI_SITE"), env("UNIFI_VERSION"), false);
-        $login            = $unifi_connection->login();
-        $results          = $unifi_connection->custom_api_request('/api/s/' . env("UNIFI_SITE") . '/rest/firewallrule/' . $id, $method);
-
-        return $results;
-    }
-
-    private function updaterule($id, $rule)
-    {
-        $method = 'PUT';
-
-        $unifi_connection = new Unifi_Client(env("UNIFI_USER"), env("UNIFI_PASS"), env("UNIFI_URI"), env("UNIFI_SITE"), env("UNIFI_VERSION"), false);
-        $login            = $unifi_connection->login();
-        $results          = $unifi_connection->custom_api_request('/api/s/' . env("UNIFI_SITE") . '/rest/firewallrule/' . $id, $method, $rule);
-
-        return $results;
-
-    }
-
 }
