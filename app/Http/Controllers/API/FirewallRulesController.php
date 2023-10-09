f709bf696b2693128b031376a4967193dc42cdaa
Tor Flatebo <tor@flatebo.org>
2023-10-08 20:24:36 -0500

    Implement persistent login using the cookie that the UnifiClient pulls from the Unifi API. Store it in the global PHP session, otherwise it won't resuse it.

--- app/Http/Controllers/API/FirewallRulesController.php
+++ app/Http/Controllers/API/FirewallRulesController.php
@@ -4,67 +4,70 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Cache;
+
 
 use UniFi_API\Client as Unifi_Client;
 
 class FirewallRulesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    protected $unifi_connection;
+
+    public function __construct()
     {
-        //
-        return response()->json($this -> listrules());
+        // Create a connection that can be reused
+        $this->unifi_connection =
+            new Unifi_Client(
+                env("UNIFI_USER"),
+                env("UNIFI_PASS"),
+                env("UNIFI_URI"),
+                env("UNIFI_SITE"),
+                env("UNIFI_VERSION"),
+                false
+            );
+
+        // check if we have a cookie from a previous API call, if so, set it on the 
+        // client and in the global PHP session
+        if(Cache::has('unificookie'))
+        {             
+            Log::debug("Setting cookie in client from cache in constructor");
+
+            $this->unifi_connection->set_cookies(Cache::get('unificookie'));
+
+            // This is the all-important line, without this it will not 
+            // use the cookie
+            $_SESSION['unificookie'] = Cache::get('unificookie');
+        }
+        else
+        {
+            Log::debug("Cookie was not found in cache");
+        }
+
+        // prime the client by calling login
+        if($this->unifi_connection->login())
+        { // have a valid login
+                        
+            Log::debug("Valid login in controller constructor");
+            Log::debug($this->unifi_connection->get_cookies());
+
+            Log::debug("Store cookie in cache in constructor");
+            Cache::put('unificookie', $this->unifi_connection->get_cookies());
+        }
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    * Display a listing of firewall rules
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function index(Request $request)
     {
-        // not used
+        return $this->unifi_connection->list_firewallrules();
     }
-
+    
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        // not used
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        return response()->json($this -> getrule($id));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        // not used
-    }
-
-    /**
-     * Update the specified resource in storage.
+     * Update the specified firewall rule
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
@@ -73,54 +76,31 @@ class FirewallRulesController extends Controller
     public function update(Request $request, $id)
     {
         $payload = array('enabled' => ($request->json()->get('enabled')));
-        return response()->json($this -> updaterule($id, $payload));
-    }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        // not used
-    }
+        $method = 'PUT';
 
-    private function listrules()
-    {
-        /**
-         * initialize the Unifi API connection class, log in to the controller and request the alarms collection
-         * (this example assumes you have already assigned the correct values to the variables used)
-         */
-        $unifi_connection = new Unifi_Client(env("UNIFI_USER"), env("UNIFI_PASS"), env("UNIFI_URI"), env("UNIFI_SITE"), env("UNIFI_VERSION"), false);
-        $login            = $unifi_connection->login();
-        $results          = $unifi_connection->list_firewallrules(); // returns a PHP array containing alarm objects
+        $results = $this->
+            unifi_connection->
+                custom_api_request
+                (
+                    '/api/s/' . env("UNIFI_SITE") . '/rest/firewallrule/' . $id, 
+                    $method, 
+                    $payload
+                );
 
         return $results;
     }
 
+    /**
+     * Get an individual firewall rule
+     */
     private function getrule($id)
     {
         $method = "GET";
 
-        $unifi_connection = new Unifi_Client(env("UNIFI_USER"), env("UNIFI_PASS"), env("UNIFI_URI"), env("UNIFI_SITE"), env("UNIFI_VERSION"), false);
-        $login            = $unifi_connection->login();
-        $results          = $unifi_connection->custom_api_request('/api/s/' . env("UNIFI_SITE") . '/rest/firewallrule/' . $id, $method);
+        $results = $this->unifi_connection->custom_api_request('/api/s/' . env("UNIFI_SITE") . '/rest/firewallrule/' . $id, $method);
 
         return $results;
-    }
-
-    private function updaterule($id, $rule)
-    {
-        $method = 'PUT';
-
-        $unifi_connection = new Unifi_Client(env("UNIFI_USER"), env("UNIFI_PASS"), env("UNIFI_URI"), env("UNIFI_SITE"), env("UNIFI_VERSION"), false);
-        $login            = $unifi_connection->login();
-        $results          = $unifi_connection->custom_api_request('/api/s/' . env("UNIFI_SITE") . '/rest/firewallrule/' . $id, $method, $rule);
-
-        return $results;
-
     }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\API\FirewallRulesController;
+use App\Http\Middleware\MyUnifiClient;
 
 /*
 |--------------------------------------------------------------------------
